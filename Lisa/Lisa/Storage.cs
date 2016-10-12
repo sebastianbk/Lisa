26a35e5427eef1c9ea41b1d1504ebbc30f8cb4b8
Bart Jansen <bajansen@microsoft.com>
2016-10-12 15:50:33 +0200

    added powerbi stuff

--- Lisa/Lisa/Storage.cs
+++ Lisa/Lisa/Storage.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Diagnostics;
 
 namespace Lisa
 {
@@ -47,6 +51,26 @@ namespace Lisa
 
             TableOperation insertOperation = TableOperation.Insert(sentimentScore);
             table.Execute(insertOperation);
+
+
+            #region powerbi
+            var streamingDataUrl = "https://api.powerbi.com/beta/72f988bf-86f1-41af-91ab-2d7cd011db47/datasets/6b48c5da-43d4-4eac-8bb3-323a27e18e49/rows?key=73oi5Z3r7UElJD5ldjTFOz5oLzC4tuFTGO8bOUspVDeQ4HtyL82rywNK2FFNXugM9qa9Wz9PO3l81lt%2B451lSg%3D%3D";
+
+
+            var sentimentObject = new
+            {
+                date = DateTime.Now.ToString("o"),
+                sentiment = score
+            };
+
+            var json = JsonConvert.SerializeObject(sentimentObject);
+
+            var client = new HttpClient();
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            client.PostAsync(streamingDataUrl, content);
+
+            #endregion
+
         }
     }
 }
