c22810b493432fd03e19c2b86f991bc8a7f96ddb
svandenhoven <svandenhoven@hotmail.com>
2016-10-12 15:26:25 +0200

    First Version

---
+++ Lisa/Lisa/Phychologist.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Headers;
+
+namespace Lisa
+{
+    public class Phychologist
+    {
+        public static async Task<double> GetSentiment(string inputMsg)
+        {
+            var sentimentScore = 0.5;
+            string queryUri = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+            string apiKey = ConfigurationManager.AppSettings["MicrosoftCSTextAppKey"];
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            BatchInput sentimentInput = new BatchInput();
+            sentimentInput.documents = new List<Lisa.DocumentInput>();
+            sentimentInput.documents.Add(new DocumentInput()
+                {
+                    id = 1,
+                    text = inputMsg
+                }
+            );
+
+            var sentimentJsonInput = JsonConvert.SerializeObject(sentimentInput);
+            byte[] byteData = Encoding.UTF8.GetBytes(sentimentJsonInput);
+
+            var content = new ByteArrayContent(byteData);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var sentimentPost = await client.PostAsync(queryUri, content);
+
+            var sentimentRawResponse = await sentimentPost.Content.ReadAsStringAsync();
+            var sentimentJsonResponse = JsonConvert.DeserializeObject<BatchResult>(sentimentRawResponse);
+
+            sentimentScore = sentimentJsonResponse.documents[0].score;
+            return sentimentScore;
+        }
+    }
+}
---
+++ Lisa/Lisa/Storage.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lisa
+{
+    public class  SentimentScore : TableEntity
+    {
+        public SentimentScore()
+        {
+            this.PartitionKey = "SentimentScores";
+            this.RowKey = DateTime.Now.Ticks.ToString();
+        }
+        public string Message { get; set; }
+        public string Session { get; set; }
+        public string Id { get; set; }
+        public double Score { get; set; }
+        public string User { get; set; }
+        public string UserId { get; set; }
+        public string UserServiceUrl { get; set; }
+
+    }
+    public class Storage
+    {
+        public static void Store(string msg, string session, double score, string user, string userId, string userServiceUrl)
+        {
+            var sentimentScore = new SentimentScore()
+            {
+                Message = msg,
+                Session = session,
+                Id = Guid.NewGuid().ToString(),
+                Score = score,
+                User = user,
+                UserId = userId,
+                UserServiceUrl = userServiceUrl          
+            };
+
+            var connectionString = CloudConfigurationManager.GetSetting("AzureWebJobsStorage");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("SessionSentiment");
+            table.CreateIfNotExists();
+
+            TableOperation insertOperation = TableOperation.Insert(sentimentScore);
+            table.Execute(insertOperation);
+        }
+    }
+}
